741ef3ffdc4697caa92c94773acec773708ced4f
ArchUsr64 <anshulusr@gmail.com>
2023-08-12 10:30:29 +0530

    added TCP handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{BufWriter, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut writer = BufWriter::new(&mut stream);
+    writer.write_all("HTTP/1.1 200 OK\n".as_bytes()).unwrap();
+    writer
+        .write_all("Content-Type: text/html\n".as_bytes())
+        .unwrap();
+    writer.write_all("\nHello World\n".as_bytes()).unwrap();
+
+    writer.flush().unwrap();
+}
+
 fn main() {
+    let listener = TcpListener::bind("[::]:8080").unwrap();
+    for stream in listener.incoming() {
+        println!("Incoming connection from: {stream:?}");
+        std::thread::spawn(move || handle_connection(stream.unwrap()));
+    }
     println!("Hello, world!");
 }
